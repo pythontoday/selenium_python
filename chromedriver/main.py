06c95d082bc4f324b49d46570e1474a2278cf231
pythontoday <pythontodaynet@gmail.com>
2020-12-21 16:31:58 +0700

    add part 2

--- chromedriver/main.py
+++ chromedriver/main.py
@@ -1,12 +1,36 @@
 from selenium import webdriver
 import time
+import random
+from fake_useragent import UserAgent
 
-url = "https://www.instagram.com/"
-driver = webdriver.Chrome(executable_path="/home/cain/PycharmProjects/selenium_python/chromedriver/chromedriver")
+# url = "https://www.instagram.com/"
+
+user_agents_list = [
+    "hello_world",
+    "best_of_the_best",
+    "python_today"
+]
+
+# options
+options = webdriver.ChromeOptions()
+
+# change useragent
+useragent = UserAgent()
+
+# options.add_argument("user-agent=HelloWorld:)")
+# options.add_argument("user-agent=Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36")
+# options.add_argument(f"user-agent={random.choice(user_agents_list)}")
+options.add_argument(f"user-agent={useragent.random}")
+
+driver = webdriver.Chrome(
+    executable_path="/home/cain/PycharmProjects/selenium_python/chromedriver/chromedriver",
+    options=options
+)
 # "C:\\users\\selenium_python\\chromedriver\\chromedriver.exe"
+# r"C:\users\selenium_python\chromedriver\chromedriver.exe"
 
 try:
-    driver.get(url=url)
+    driver.get(url="https://www.whatismybrowser.com/detect/what-is-my-user-agent")
     time.sleep(5)
     # driver.get(url="https://stackoverflow.com/")
     # time.sleep(5)
--- firefoxdriver/main.py
+++ firefoxdriver/main.py
@@ -1,13 +1,26 @@
 from selenium import webdriver
 import time
+from fake_useragent import UserAgent
 
-url = "https://www.vk.com/"
-driver = webdriver.Firefox(executable_path="/home/cain/PycharmProjects/selenium_python/firefoxdriver/geckodriver")
+# url = "https://www.vk.com/"
+
+# options
+options = webdriver.FirefoxOptions()
+
+# change useragent
+useragent = UserAgent()
+options.set_preference("general.useragent.override", useragent.random)
+
+driver = webdriver.Firefox(
+    executable_path="/home/cain/PycharmProjects/selenium_python/firefoxdriver/geckodriver",
+    options=options
+)
 # "C:\\users\\selenium_python\\chromedriver\\chromedriver.exe"
+# r"C:\users\selenium_python\chromedriver\chromedriver.exe"
 
 try:
-    driver.get(url=url)
-    driver.save_screenshot("vk.png")
+    driver.get(url="https://www.whatismybrowser.com/detect/what-is-my-user-agent")
+    # driver.save_screenshot("vk.png")
     time.sleep(5)
     # driver.get(url="https://stackoverflow.com/")
     # time.sleep(5)
